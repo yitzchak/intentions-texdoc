33aade7d06e32b5954d22253e13beadd1a1e211a
Tarn Burton <twburton@gmail.com>
2016-08-22 16:41:56 -0400

    Remove path setting in scripts

--- script/travis-ci-build.sh
+++ script/travis-ci-build.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  export PATH=/Library/TeX/texbin:$PATH
-  texdoc -l -M graphicx
-  texdoc -l -M article
-fi
-
 curl -s https://raw.githubusercontent.com/atom/ci/master/build-package.sh | sh
