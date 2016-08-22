284077a4b086cb9c3c44b297e20c97d03010ffb0
Tarn Burton <twburton@gmail.com>
2016-08-22 06:11:20 -0400

    Debug Travis CI OSX

--- script/travis-ci-before-install.sh
+++ script/travis-ci-before-install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  wget http://mirror.ctan.org/systems/mac/mactex/BasicTeX.pkg -O "/tmp/BasicTeX.pkg";
-  sudo installer -pkg "/tmp/BasicTeX.pkg" -target /;
-  rm /tmp/BasicTeX.pkg;
-  export PATH=/usr/texbin:$PATH;
-  sudo tlmgr update --self;
-  sudo tlmgr install texdoc;
+  wget http://mirror.ctan.org/systems/mac/mactex/BasicTeX.pkg -O "/tmp/BasicTeX.pkg"
+  sudo installer -pkg "/tmp/BasicTeX.pkg" -target
+  rm /tmp/BasicTeX.pkg
+  export PATH=/usr/texbin:$PATH
+  texdoc --help
+  sudo tlmgr update --self
+  sudo tlmgr install texdoc
+  texdoc --help
 fi
