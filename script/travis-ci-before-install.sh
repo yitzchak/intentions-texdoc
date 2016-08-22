dab89a8ed19ce57a8c946f8622e876ccd035c4db
Tarn Burton <twburton@gmail.com>
2016-08-22 06:12:17 -0400

    Fix target

--- script/travis-ci-before-install.sh
+++ script/travis-ci-before-install.sh
@@ -2,7 +2,7 @@
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   wget http://mirror.ctan.org/systems/mac/mactex/BasicTeX.pkg -O "/tmp/BasicTeX.pkg"
-  sudo installer -pkg "/tmp/BasicTeX.pkg" -target
+  sudo installer -pkg "/tmp/BasicTeX.pkg" -target /
   rm /tmp/BasicTeX.pkg
   export PATH=/usr/texbin:$PATH
   texdoc --help
